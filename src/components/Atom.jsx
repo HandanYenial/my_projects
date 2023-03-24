c50b5503cc62e2c7eaadfbb93053d9c86a970c4b
Your Name <you@example.com>
2023-03-23 22:18:13 -0400

    initial commit

---
+++ src/components/Atom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Atom() {
+  return (
+    <div>Atom</div>
+  )
+}
+
+export default Atom
---
+++ src/components/Shoe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Shoe() {
+  return (
+    <div>Shoe</div>
+  )
+}
+
+export default Shoe
