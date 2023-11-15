32c71e0ebfdb713c2809a28d1c30840ebbff976b
Your Name <you@example.com>
2023-11-15 12:33:58 -0500

    updates

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,16 +5,16 @@ import styled from "styled-components";
 //import Scene from "./Scene";
 
 const Desc = styled.div`
-  width: 300px;
-  height: 200px;
+  width: 350px;
+  height: 300px;
   padding: 20px;
-  background-color:#C7F5FE;
-  color:#A31ACB;
+  background-color:#C0DBEA;
+  color:#900C3F;
   border-radius: 10px;
   position: absolute;
-  font-size:large;
-  top: 30px;
-  right: 500px;
+  font-size:x-large;
+  top: 80px;
+  right: 300px;
 
   @media only screen and (max-width: 768px) {
     top: 0;
@@ -25,23 +25,15 @@ const Desc = styled.div`
   }
 `;
 
+
 const Events = () => {
   return (
     <>
-      <Canvas>
-
-      <Stage environment="city" intensity={0.6}>
-       
-       {/* "Themis' Scales of Justice" (https://skfb.ly/ozINO) by Incg5764 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). */}
-        {/* <Scene/> */}
-       </Stage>
-         {/* Added the 3D model as Scene but it seems only a black shadow--needs light--I can use Stage for that */}
-         <OrbitControls enableZoom={false}/>
-     
-      </Canvas>
+    
       <Desc>
       Events is designed to access and event information in a specific area. Users can signup, login,search for the events in their area, add their favorite event to their personalized page and leave a comment about it. This app is built with Python, Flask, TicketmasterAPI, Bootstrap, Jinja2,HTML,CSS, and PostgreSQL.
       </Desc>
+     
     </>
   );
 };
--- src/components/GlobalLegal.jsx
+++ src/components/GlobalLegal.jsx
@@ -6,21 +6,21 @@ import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 
 const Desc = styled.div`
-  width: 300px;
-  height: 150px;
+  width: 350px;
+  height: 250px;
   padding: 20px;
-  background-color: #FCC8F8;
+  background-color: #D1D1D1;
   color:#4D089A;
   border-radius: 10px;
-  font-size:large;
+  font-size:x-large;
   position: absolute;
-  bottom: 100px;
-  right: 200px;
+  bottom: 300px;
+  right: 300px;
 
   @media only screen and (max-width: 768px) {
     top: 0;
     bottom: 0;
-    left: 0;
+    left: 0;s
     right: 0;
     margin: auto;
   }
@@ -29,17 +29,6 @@ const Desc = styled.div`
 const GlobalLegal = () => {
   return (
     <>
-      <Canvas>
-
-        <Stage environment="city" intensity={0.6}>
-       
-        {/* "Themis' Scales of Justice" (https://skfb.ly/ozINO) by Incg5764 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). */}
-         {/* <Lion/> */}
-        </Stage>
-          {/* Added the 3D model as Scene but it seems only a black shadow--needs light--I can use Stage for that */}
-          <OrbitControls enableZoom={false}/>
-      
-      </Canvas>
       <Desc>
       Global&Legal is designed for law firms to build a better communication among practices. Users can signup, login, display each practice and cases associated within. 
       Node.js, Express.js,React.js, MaterialUI, Bootstrap, PostgreSQL, Heroku
